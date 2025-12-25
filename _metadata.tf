2068f9524b3ff5246f4003d4f905160a4889cd23
Bernd Herding <bernd.herding@external.biontech.com>
2025-12-25 01:53:22 +0100

    remove environment from metadata

--- _metadata.tf
+++ _metadata.tf
@@ -14,20 +14,18 @@ variable "_metadata" {
   type = object({
     actor       = string # Github actor (deployer) of the deployment
     catalog_id  = string # SLT catalog id of this module
-    environment = string # environment of the deployment, defaults to <actor>
-    deployment  = string # slt-<catalod_id>-<repo>-<environment>
+    deployment  = string # slt-<catalod_id>-<repo>-<actor>>
     ref         = string # Git reference of the deployment
     ref_name    = string # Git ref_name (branch) of the deployment
     repo        = string # GitHub short repository name (without owner) of the deployment
     repository  = string # GitHub full repository name (including owner) of the deployment
     sha         = string # Git (full-length, 40 char) commit SHA of the deployment
-    short_name  = string # slt-<cat>-<environment>
+    short_name  = string # slt-<cat>-<actor>
     time        = string # Timestamp of the deployment
   })
   default = {
     actor       = ""
     catalog_id  = ""
-    environment = ""
     deployment  = ""
     ref         = ""
     ref_name    = ""
